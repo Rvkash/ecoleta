d30c718ca3d2babede8606e584ae007e4d65dca4
Rukash <rafael.ieel@hotmail.com>
2020-06-12 23:46:57 -0300

    add selected items and filtered items

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -13,12 +13,19 @@ function populateUFs () {
 populateUFs()
 
 function getCities (event) {
-  const citySelect = document.querySelector('select[name=city]')
+	const citySelect = document.querySelector('select[name=city]')
+	const stateInput = document.querySelector('input[name=state]')
 
-  const ufValue = event.target.value
+
+	const ufValue = event.target.value
+
+	const indexOfSelectedState = event.target.selectedIndex
+	stateInput.value = event.target.options[indexOfSelectedState].text
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+	citySelect.innerHTML = "<option value>Selecione a cidade</option>"
+	citySelect.disable = true
 
   fetch(url)
     .then(res => res.json())
@@ -34,3 +41,50 @@ function getCities (event) {
 document
   .querySelector('select[name=uf]')
   .addEventListener('change', getCities)
+
+
+  // items de coleta
+
+  const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+  for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+
+  }
+
+  const collectedItems = document.querySelector("input[name=items]")
+
+  let selectedItems = []
+
+  function handleSelectedItem(event) {
+    const itemLi = event.target
+
+    //Adicionar ou remover uma classe com javascript
+    itemLi.classList.toggle("selected")
+    
+    const itemId = itemLi.dataset.id
+
+    const alreadySelected = selectedItems.findIndex( item => {
+      const itemFound = item == itemId // isso será true ou false
+      return itemFound
+  })
+
+  if( alreadySelected >= 0 ) {
+      // tirar da selecao
+      const filteredItems = selectedItems.filter( item => {
+          const itemIsDifferent = item != itemId // false
+          return itemIsDifferent
+      })
+
+      selectedItems = filteredItems
+  }else {
+    selectedItems.push(itemId)
+
+    console.log(selectedItems)
+  }
+
+  collectedItems.value = selectedItems
+
+  
+
+}
